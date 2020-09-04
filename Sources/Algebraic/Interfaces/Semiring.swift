5b1d1c190c270838bad94751bb9922689f25f8ea
José Manuel Sánchez <buscarini@gmail.com>
2020-09-04 12:20:26 +0200

    Added semiring. Added function to monoid without wrapper

---
+++ Sources/Algebraic/Interfaces/Semiring.swift
@@ -0,0 +1,27 @@
+//
+//  Semiring.swift
+//  Algebraic
+//
+//  Created by José Manuel Sánchez Peñarroja on 04/09/2020.
+//
+
+import Foundation
+
+public struct Semiring<T> {
+	public var zero: T
+	public var one: T
+	public var add: (T, T) -> T
+	public var mult: (T, T) -> T
+	
+	public init(
+		zero: T,
+		one: T,
+		add: @escaping (T, T) -> T,
+		mult: @escaping (T, T) -> T
+	) {
+		self.zero = zero
+		self.one = one
+		self.add = add
+		self.mult = mult
+	}
+}
